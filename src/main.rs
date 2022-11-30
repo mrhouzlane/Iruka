e29e5131e92b740e650bf3a31ea12775a15ed964
mehdi <rhouzm@gmail.com>
2022-11-30 23:44:05 +0100

    feat : orderbook

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+enum BidOrAsk {
+    Bid,
+    Ask,
 }
+
+struct Price {
+    integral: u64,
+    fractional: u64,
+    scalar: u64,
+}
+
+impl Price {
+    fn new(price: f64) -> Price {
+        let scalar = 100000;
+        let integral = price as u64;
+        let fractional = (price % 1.0) * scalar as f64;
+        Price {
+            scalar,
+            integral,
+            fractional,
+        }
+    }
+}
+
+struct Limit {
+    price: f64,
+    orders: Vec<Order>,
+}
+
+struct Order {
+    size: f64,
+    bid_or_ask: BidOrAsk,
+}
+
+impl Order {
+    fn new(bid_or_ask: BidOrAsk, size: f64) -> Order {
+        Order { bid_or_ask, size }
+    }
+}
+
+fn main() {}
---
+++ target/debug/deps/shoko-6e6404f9664f189b.d
@@ -0,0 +1,5 @@
+/Users/mehdirhouzlane/shoko/target/debug/deps/shoko-6e6404f9664f189b.rmeta: src/main.rs
+
+/Users/mehdirhouzlane/shoko/target/debug/deps/shoko-6e6404f9664f189b.d: src/main.rs
+
+src/main.rs:
