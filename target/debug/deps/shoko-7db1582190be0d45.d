5ba2a82455f1fe928790d4f1b3f57ce2d3f83d57
mehdi <rhouzm@gmail.com>
2022-12-01 04:13:56 +0100

    feat : structure

---
+++ target/debug/deps/shoko-7db1582190be0d45.d
@@ -0,0 +1,5 @@
+/Users/mehdirhouzlane/shoko/target/debug/deps/shoko-7db1582190be0d45: src/main.rs
+
+/Users/mehdirhouzlane/shoko/target/debug/deps/shoko-7db1582190be0d45.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/shoko.d
@@ -0,0 +1,1 @@
+/Users/mehdirhouzlane/shoko/target/debug/shoko: /Users/mehdirhouzlane/shoko/src/main.rs
